e929b7e38d00b6ae385f96062f46ad7c80f4ab32
romainf28 <romain.fournier@student-cs.fr>
2023-04-11 15:37:11 +0200

    get string representation of predicates in the initial state

--- grounder.py
+++ grounder.py
@@ -24,6 +24,10 @@ class Grounder:
         # Get the static predicates
         static_predicates = self._get_static_predicates()
 
+        # Get string representation of the atoms in the initial state
+        initial_state = self._get_partial_state(
+            self.problem.initial_state.predicates)
+
     def _get_static_predicates(self):
         """
         Returns the list of static predicates, i.e. predicates which
@@ -48,3 +52,16 @@ class Grounder:
         static_predicates = [pred[0]
                              for pred in self.predicates if is_static(pred)]
         return static_predicates
+
+    def _get_grounded_string(self, name, args):
+        """Return a more convenient string representation for a predicate"""
+        args_string = " " + " ".join(args) if args else ""
+        return "({}{})".format(name, args_string)
+
+    def _get_fact(self, atom):
+        """Return the string representation of a grounded atom."""
+        return self._get_grounded_string(atom[0], atom[1:])
+
+    def _get_partial_state(self, atoms):
+        """Return a set of the string representation of the grounded atoms."""
+        return frozenset(self._get_fact(atom) for atom in atoms)
