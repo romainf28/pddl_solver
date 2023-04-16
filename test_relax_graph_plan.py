9abc8611ca11a476b5c243ade9b76a838af93949
romainf28 <romain.fournier@student-cs.fr>
2023-04-16 17:53:10 +0200

    solve parsing error

--- test_relax_graph_plan.py
+++ test_relax_graph_plan.py
@@ -4,15 +4,14 @@ import pddlparser
 from GraphPlanRelaxed import GraphPlanRelaxed
 
 
-domain_file = 'instances/groupe1/domain.pddl'
-problem_file = 'instances/groupe1/problem0.pddl'
+domain_file = 'instances/groupe3/domain.pddl'
+problem_file = 'instances/groupe3/problem1.pddl'
 
 domain = pddlparser.PDDLParser.parse(domain_file)
 problem = pddlparser.PDDLParser.parse(problem_file)
 
 grounder = Grounder(domain, problem)
 task = grounder.ground()
-
 
 
 graph_plan = GraphPlanRelaxed(task)
